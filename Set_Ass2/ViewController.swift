c90222937bae7ce2327d392f3fd82f4261213499
Jim Kardach <jim@kardach.clom>
2021-05-20 20:05:20 -0700

    Initial Commit

---
+++ Set_Ass2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Set_Ass2
+//
+//  Created by jim kardach on 5/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
